7c686f8d5aa120e5e140c952c957508b3a1124ce
nsuriya239 <surya@Suryas-MacBook-Pro.local>
2023-07-18 23:07:45 -0400

    Added Add btn and cost details for the carousel 🎨

--- src/components/RestaurantDetailsComponent/MenuComponent/MenuListComponent/CarouselComponent/index.js
+++ src/components/RestaurantDetailsComponent/MenuComponent/MenuListComponent/CarouselComponent/index.js
@@ -1,31 +1,55 @@
-import { CAROUSEL_IMG_CDN_BASE_URL } from '../../../../../utils/constants';
-import './index.css';
+import { CAROUSEL_IMG_CDN_BASE_URL } from "../../../../../utils/constants";
+import "./index.css";
 
 const CarouselCardComponent = (props) => {
-    const {card} = props;
-    return (
-        <div className='carousel-card'>
-            <img src={CAROUSEL_IMG_CDN_BASE_URL+card?.creativeId} />
+  const { card } = props;
+  const { creativeId, dish } = card;
+  return (
+    <div className="carousel-card">
+      <img src={CAROUSEL_IMG_CDN_BASE_URL + creativeId} />
+      <div className="price-and-add-btn">
+        <div className="price">₹ {(dish?.info?.price ? dish?.info?.price : dish?.info?.defaultPrice) / 100}</div>
+        <div className="add-outOfStock-btn">
+          {dish?.info?.inStock === 1 ? (
+            <div className="add">
+              <div>Add</div>
+              {dish?.info?.addons ? <div className="customisable-plus-sign">+</div> : null}
+            </div>
+          ) : (
+            <div className="outOfStock">
+              {dish?.info?.nextAvailableAtMessage ? dish?.info?.nextAvailableAtMessage : "Out of stock"}
+            </div>
+          )}
+          {dish?.info?.addons ? <div className="customisable">Customisable</div> : null}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 // TODO: Need to add Price and Add button to the carousel card
 
 const CarouselComponent = (props) => {
-    const {categoryData} = props;
-    console.log(categoryData);
-    return (
-        <div className='carousel-category-container'>
-        <div className='carousel-category-header'>
-            <h2>{categoryData?.title}</h2>
-        </div>
-        <div className='carousel-wrapper'>
-            {categoryData?.carousel?.map(carouselCard => <CarouselCardComponent key={carouselCard?.bannerId} card={carouselCard}/>)}
-        </div>
-        <div className="category-border" />
-        </div>
-    )
+  const { categoryData } = props;
 
-}
+  console.log(categoryData);
+  return (
+    <div className="carousel-category-container">
+      <div className="carousel-category-header">
+        <h2>{categoryData?.title}</h2>
+      </div>
+      <div className="carousel-wrapper">
+        {categoryData?.carousel?.map((carouselCard) => (
+          <CarouselCardComponent key={carouselCard?.bannerId} card={carouselCard} />
+        ))}
+      </div>
+      <div className="carousel-indicators">
+        {categoryData?.carousel?.map((carouselCard) => (
+          <div key={carouselCard?.bannerId} id={carouselCard?.bannerId} className="indicator"></div>
+        ))}
+      </div>
+      <div className="category-border" />
+    </div>
+  );
+};
 
 export default CarouselComponent;
